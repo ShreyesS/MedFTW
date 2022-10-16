f4793880a20e48041c1647028d2c8c923fc491df
Shreyes Sridhara <shreyesXsridhara@gmail.com>
2022-10-15 22:14:22 -0700

    added nav menu pages

---
+++ lib/app_screens/navmenu.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:tush/app_screens/UpcomingInternships.dart';
+import 'package:tush/app_screens/screens.dart';
+import 'dart:async';
+
+import 'package:tush/app_screens/upcomingEvents.dart';
+
+class BottomNavScreen extends StatefulWidget {
+  @override
+  _BottomNavScreenState createState() => _BottomNavScreenState();
+}
+
+class _BottomNavScreenState extends State<BottomNavScreen> {
+  final List _screens = [
+    HomeScreen(),
+    StudyMaterials(),
+    UpcomingEvents(),
+    InternshipWidget(),
+    Tipsforstudents(),
+  ];
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        elevation: 0.0,
+        items: [
+          Icons.home,
+          Icons.menu_book_outlined,
+          Icons.event_available,
+          Icons.work,
+          Icons.face_retouching_natural
+        ]
+            .asMap()
+            .map((key, value) => MapEntry(
+                  key,
+                  BottomNavigationBarItem(
+                    title: Text(''),
+                    icon: Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 6.0,
+                        horizontal: 16.0,
+                      ),
+                      decoration: BoxDecoration(
+                        color: _currentIndex == key
+                            ? Colors.blue[600]
+                            : Colors.transparent,
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Icon(value),
+                    ),
+                  ),
+                ))
+            .values
+            .toList(),
+      ),
+    );
+  }
+}
---
+++ lib/app_screens/upcomingEvents.dart
@@ -0,0 +1,188 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class UpcomingEvents extends StatelessWidget {
+  _launchURLmun() async {
+    const url = 'https://munlist.com/';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    var events = {
+      1: "Bilkent Historical MUN",
+      2: "IMPRINT MUN 3.0",
+      3: "Middle Eastern MUN",
+      4: "WORLD YOUTH CONVOCATION MUN",
+      5: "IMMORTAL YOUTH MUN",
+      6: "Orestecia Summit",
+      7: "InterMUN",
+      8: "Conrad Open Forum",
+      9: "Stay At Home Model United Nations 2021",
+      10: "International Crisis Summit MUN 2021"
+    };
+    return Scaffold(
+      backgroundColor: Colors.orange,
+    
+        body: ListView(
+          
+          children: <Widget>[
+              Padding(padding: EdgeInsets.only(top: 50.0)),
+
+              Text("Upcoming Events",
+            textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  fontFamily: "Montserrat",
+          ),),
+
+            Padding(padding: EdgeInsets.only(top: 50.0,)),
+                    Text("MUNs Available:",
+                    textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                  fontFamily: "Montserrat",
+          ),),
+              
+              Padding(padding: EdgeInsets.only(bottom: 40.0)),
+              Text(
+                events[1],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat",
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 30.0)),
+              Text(
+                events[2],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat"
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 30.0)),
+              Text(
+                events[3],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat"
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 30.0)),
+              Text(
+                events[4],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat"
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 30.0)),
+              Text(
+                events[5],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat"
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 30.0)),
+              Text(
+                events[6],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat"
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 30.0)),
+              Text(
+                events[7],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat"
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 30.0)),
+              Text(
+                events[8],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat"
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 30.0)),
+              Text(
+                events[9],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat"
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 30.0)),
+              Text(
+                events[10],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Montserrat"
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 40.0)),
+              Container(
+                    width: 125.0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        IconButton(
+                          alignment: Alignment.bottomLeft,
+                          icon: Icon(Icons.add),
+                          iconSize: 60,
+                          color: Colors.green,
+                          onPressed: _launchURLmun,
+                        )
+                      ],
+                    )),
+              Padding(padding: EdgeInsets.only(bottom: 40.0)),
+            ],
+          ),
+        );
+  }
+}
